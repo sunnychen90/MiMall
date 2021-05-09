7fae99fd88714a6212a0bcd2a9e7db0e03171bdb
sunnychen90 <2507734048@qq.com>
2021-05-09 23:43:07 +0800

    NavHeader编写，sass-loader(8.0.0),node-sass(4.14.1降级

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>这里是头部</div>
+    <div class="header">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则 </a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;">购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header"></div>
+
+    </div>
 </template>
 
 <script>
@@ -9,6 +27,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color: #333333;
+            color: #B0B0B0;
+        }
+        .container{
+            width:1226px; 
+            margin:0 auto;
+        }
+
+    } 
 
 </style>
