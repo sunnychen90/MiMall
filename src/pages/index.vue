68c5bb59aecdf21f5f19165e31b73bfafbe37c02
sunnychen90 <2507734048@qq.com>
2021-05-11 19:36:18 +0800

    广告banner - 手机商品list页面实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -58,10 +58,46 @@
                     <div class="swiper-button-next" slot="button-next"></div>
                 </swiper>
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
-            <div class="product-box"></div>
+            <div class="ads-box">
+                <a :href="'/#/product/'+ item.id" v-for="(item, index) in adsList" :key="index">
+                    <img :src="item.img" alt="">
+                </a>
+            </div>
+
+            <div class="banner">
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+                </a>
+            </div>            
         </div>
+        <div class="product-box">
+                <div class="container">
+                    <h2>手机</h2>
+                    <div class="wrapper">
+                        <div class="banner-left">
+                            <a href="/#/product/35">
+                                <img src="/imgs/mix-alpha.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="list-box">
+                            <div class="list" v-for="(arr,index) in phoneList" :key="index">
+                                <div class="item" v-for="(item,index) in arr" :key="index">
+                                    <span>新品</span>
+                                    <div class="item-img">
+                                        <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6f2493e6c6fe8e2485c407e5d75e3651.jpg" alt="">
+                                    </div>
+                                    <div class="item-info">
+                                        <h3>小米9</h3>
+                                        <p>骁龙855，索尼4800万超广角微距</p>
+                                        <p class="price">2999元</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                </div>                
+            </div>
+
         <ServiceBar></ServiceBar>
     </div>
 </template>
@@ -148,6 +184,29 @@ export default {
                 [0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0]
+            ],
+            adsList:[
+                {
+                    id:33,
+                    img:'/imgs/ads/ads-1.png',
+                },
+                {
+                    id:48,
+                    img:'/imgs/ads/ads-2.jpg',
+                },
+                {
+                    id:45,
+                    img:'/imgs/ads/ads-3.png',
+                },
+                {
+                    id:47,
+                    img:'/imgs/ads/ads-4.jpg',
+                },
+            ],
+            phoneList:[
+                [1,1,1,1],
+                [1,1,1,1]
+
             ]
         }
     }
@@ -238,6 +297,95 @@ export default {
                 
             }
 
+        }
+    }
+    .ads-box{
+        @include flex();
+        margin-top: 14px;
+        margin-bottom: 31px;
+        a{
+            width: 296px;
+            height: 167px;
+        }        
+    }
+    .banner{
+        margin-bottom: 50px;
+    }
+    .product-box{
+        background-color: $colorJ;
+        padding: 30px 0 50px;
+        h2{
+            font-size: 22px;
+            height: 21px;
+            line-height: 21px;
+            color: $colorB;
+            margin-bottom: 20px;
+        }
+        .wrapper{
+            display: flex;
+            .banner-left{
+                margin-right: 16px;
+                img{
+                    width: 224px;
+                    height: 619px;                    
+                }
+            }
+            .list-box{
+                .list{
+                    @include flex();
+                    width: 986px;       
+                    margin-bottom: 14px;
+                    &:last-child{
+                        margin-bottom: 0;
+                    }
+                    .item{
+                        width: 236px;
+                        height: 302px;
+                        background-color: #FFFFFF;
+                        text-align: center;
+                        span{
+                            display: inline-block;
+                            width: 67px;
+                            height: 24px;
+                            font-size: 14px;
+                            line-height: 24px;
+                            color: #fff;
+                            background-color: #7ecf68;
+                        }
+                        .item-img{
+                            img{
+                                height: 195px;
+                            }
+                        }
+                        .item-info{
+                            h3{
+                                font-size: 14px;
+                                color: $colorB;
+                                line-height: 14px;
+                                font-weight: bold;
+                            }
+                            p{
+                                color: $colorD;
+                                line-height: 13px;
+                                margin: 6px auto 13px;
+                            }
+                            .price{
+                                color: #F20A0A;
+                                font-size: 14px;
+                                font-weight: bold;
+                                cursor: pointer;
+                                &:after{
+                                    content: " ";
+                                    @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                                    margin-left: 5px;
+                                    vertical-align: middle;
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     
